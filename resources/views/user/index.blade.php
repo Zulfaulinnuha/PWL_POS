914b8f8a14ba6be7ab8e8d2961198ee1df33e1e5
= <=>
2024-09-25 23:31:49 +0700

    Jobsheet 5-Praktikum 4

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -17,15 +17,25 @@
     @if (session('error'))
         <div class="alert alert-danger">{{ session('error') }}</div>
     @endif
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group row">
+            <label class="col-1 control-label col-form-label">Filter:</label>
+            <div class="col-3">
+                <select class="form-control" id="level_id" name="level_id" required>
+                <option value="">- Semua</option>
+                @foreach($level as $item)
+                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                @endforeach
+                </select>
+                <small class="form-text text-muted">Level Pengguna</small>
+            </div>
+            </div>
+        </div>
+        </div>
         <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
             <thead>
                 <tr>
-                    <th>ID</th>
-                    <th>Username</th>
-                    <th>Nama</th>
-                    <th>Level</th>
-                    <th>Aksi</th>
-                </tr>
             </thead>
         </table>
     </div>
@@ -44,7 +54,9 @@ $(document).ready(function() {
         ajax: {
             "url": "{{ url('user/list') }}",
             "type": "POST",
-            "dataType": "json",
+            "data": function (d) {
+                d.level_id = $('#level_id').val();
+            }
         },
         columns: [
            {
@@ -80,6 +92,11 @@ $(document).ready(function() {
         }
         ]
     });
+
+    $('#level_id').on('change', function() {
+        dataUser.ajax.reload();
+    });
+    
 });
 </script>
 @endpush
